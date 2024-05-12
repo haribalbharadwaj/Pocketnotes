9025a814b76cc18ea008df1298c6c95a5df7b7a8
BHARADWAJ HARIBAL <haribalbharadwaj@gmail.com>
2024-05-12 09:48:33 +0530

    Pocket notes app

---
+++ pocket-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Chat from "./components/Chat"
+
+function App() {
+
+  return (
+  
+      <div>
+        <Chat/>
+      </div>
+     
+  )
+}
+
+export default App
